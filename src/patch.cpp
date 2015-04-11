09fe34d4ec8bd4390b6267e0c36c25dd71f78315
Augustin Cavalier <waddlesplash@gmail.com>
2015-04-11 18:51:59 -0400

    Get rid of zip/gzip support.
    Multiio was just too convoluted and flaky, and zip/gzip
    compression isn't so hard for users to handle anyway. Perhaps in the
    future this'll get reimplemented in a better way.

--- src/patch.cpp
+++ src/patch.cpp
@@ -7,10 +7,6 @@ Copyright (c) 1998-2015, Charles Bilyue.
 Portions copyright (c) 1998-2003, Brad Martin.
 Portions copyright (c) 2003-2004, Daniel Horchner.
 Portions copyright (c) 2004-2005, Nach. ( http://nsrt.edgeemu.com/ )
-Unzip Technology, copyright (c) 1998 Gilles Vollant.
-zlib Technology ( www.gzip.org/zlib/ ), Copyright (c) 1995-2003,
- Jean-loup Gailly ( jloup* *at* *gzip.org ) and Mark Adler
- ( madler* *at* *alumni.caltech.edu ).
 JMA Technology, copyright (c) 2004-2005 NSRT Team. ( http://nsrt.edgeemu.com/ )
 LZMA Technology, copyright (c) 2001-4 Igor Pavlov. ( http://www.7-zip.org )
 Portions copyright (c) 2002 Andrea Mazzoleni. ( http://advancemame.sf.net )
@@ -26,7 +22,6 @@ You must read and accept the license prior to use.
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include "unzip.h"
 #include "romload.h"
 
 #define BUFFER_SIZE 2048
@@ -46,7 +41,6 @@ struct
   unsigned int buffer_total;
   unsigned int proccessed;
 
-  unzFile zipfile;
   FILE *fp;
 } static IPSPatch;
 
@@ -55,9 +49,7 @@ static bool reloadBuffer()
 {
   if (IPSPatch.proccessed == IPSPatch.file_size) { return(false); }
 
-  IPSPatch.buffer_total = IPSPatch.fp ?
-  /* Regular Files */     fread(IPSPatch.data, 1, BUFFER_SIZE, IPSPatch.fp) :
-  /* Zip Files     */     unzReadCurrentFile(IPSPatch.zipfile, IPSPatch.data, BUFFER_SIZE);
+  IPSPatch.buffer_total = fread(IPSPatch.data, 1, BUFFER_SIZE, IPSPatch.fp);
 
   IPSPatch.current = IPSPatch.data;
   if (IPSPatch.buffer_total && (IPSPatch.buffer_total <= BUFFER_SIZE))
@@ -93,8 +85,6 @@ static bool initPatch()
 
   IPSPatch.proccessed = 0;
 
-  IPSPatch.zipfile = 0;
-
   IPSPatch.fp = 0;
   IPSPatch.fp = fopen(patchfile, "rb");
   if (!IPSPatch.fp) { return(false); }
@@ -115,13 +105,6 @@ static void deinitPatch()
     fclose(IPSPatch.fp);
     IPSPatch.fp = 0;
   }
-
-  if (IPSPatch.zipfile)
-  {
-    unzCloseCurrentFile(IPSPatch.zipfile);
-    unzClose(IPSPatch.zipfile);
-    IPSPatch.zipfile = 0;
-  }
 }
 
 
@@ -132,13 +115,7 @@ void PatchUsingIPS()
 
   IPSPatched = false;
 
-  if (!AutoPatch)
-  {
-    deinitPatch(); //Needed if the call to this function was done from findZipIPS()
-    return;
-  }
-
-  if (patchfile) //Regular file, not Zip
+  if (patchfile)
   {
     if (!initPatch())
     {
@@ -222,63 +199,4 @@ void PatchUsingIPS()
   fwrite(RomAddress, 1, rom_allocated_size, fp);
   fclose(fp);
   */
-}
-
-void findZipIPS(const char *compressedfile)
-{
-  bool FoundIPS = false;
-  unz_file_info cFileInfo; //Create variable to hold info for a compressed file
-  int cFile;
-
-  IPSPatch.zipfile = unzOpen(compressedfile); //Open zip file
-  cFile = unzGoToFirstFile(IPSPatch.zipfile); //Set cFile to first compressed file
-
-  while(cFile == UNZ_OK) //While not at end of compressed file list
-  {
-    //Temporary char array for file name
-    char cFileName[256];
-
-    //Gets info on current file, and places it in cFileInfo
-    unzGetCurrentFileInfo(IPSPatch.zipfile, &cFileInfo, cFileName, 256, NULL, 0, NULL, 0);
-
-    //Find IPS file
-    if (strlen(cFileName) >= 5) //Char + ".IPS"
-    {
-      char *ext = cFileName+strlen(cFileName)-4;
-      if (!strncasecmp(ext, ".IPS", 4))
-      {
-        FoundIPS = true;
-        break;
-      }
-    }
-
-    //Go to next file in zip file
-    cFile = unzGoToNextFile(IPSPatch.zipfile);
-  }
-
-  if (!FoundIPS)
-  {
-    unzClose(IPSPatch.zipfile);
-    IPSPatch.zipfile = 0;
-    return;
-  }
-
-  //Open file
-  unzOpenCurrentFile(IPSPatch.zipfile);
-
-  patchfile = 0;
-  IPSPatch.fp = 0;
-  IPSPatch.file_size = (unsigned int)cFileInfo.uncompressed_size;
-  IPSPatch.data = (unsigned char *)malloc(BUFFER_SIZE);
-  if (IPSPatch.data)
-  {
-    IPSPatch.proccessed = 0;
-    reloadBuffer();
-    printf("Patching ROM with IPS that was in the ZIP file.\n");
-    PatchUsingIPS();
-  }
-  else
-  {
-    deinitPatch();
-  }
 }
